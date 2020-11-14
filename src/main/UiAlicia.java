64402ab2c230b277ac29bbad1944a2b090132989
himikokuronaga <hugolanguila@gmail.com>
2020-11-14 15:54:49 -0600

    Controles Cifrar/Decifrar

--- src/main/UiAlicia.java
+++ src/main/UiAlicia.java
@@ -6,18 +6,22 @@ import javax.swing.event.*;
 
 public class UiAlicia extends JFrame{
 
-	private JLabel nombreArchivo;
-	private JButton btnCifrar;
+	private JLabel lblTitulo;
+	private JLabel lblNombreArchivo;
+	private JButton btnOperacion;
 	private JButton btnSeleccionar;
+	private JCheckBox cbxCifrar;
+	private JCheckBox cbxDecifrar;
 
 	private boolean seleccionado;
+	private boolean opCifrar;
+	private boolean opDecifrar;
 	private JFileChooser fc;
-
 	private File archivo;
 
 	public UiAlicia(){
 		setTitle("Alicia");	
-		setSize(480, 240);	
+		setSize(520, 240);	
 		setVisible(true);
 		/*Cuando se cierra la ventana*/
 		addWindowListener(new WindowAdapter(){
@@ -25,36 +29,92 @@ public class UiAlicia extends JFrame{
 				accionSalir();
 			}
 		});
-
 		init();
 	}
 
 	void init(){
 
-		JPanel panel = new JPanel();	
+		JPanel panelSuperior = new JPanel();	
+		JPanel panelArchivo = new JPanel();
+		JPanel panelControles = new JPanel();
 		
-		nombreArchivo = new JLabel("No has seleccionado nada");
+		lblTitulo = new JLabel("Modos de operacion");
+		lblNombreArchivo = new JLabel("Selecciona un archivo", SwingConstants.CENTER);
 		
-		btnCifrar = new JButton("Cifrar");
-		btnCifrar.addActionListener( new ActionListener(){
-			public void actionPerformed( ActionEvent e ){
-				accionCifrar();
-			}	
-		});
-
 		btnSeleccionar = new JButton("Seleccionar");
 		btnSeleccionar.addActionListener( new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				accionSeleccionar();
 			}	
 		});
+		
+		btnOperacion = new JButton("Cifrar/Decifrar");
+		btnOperacion.addActionListener( new ActionListener(){
+			public void actionPerformed( ActionEvent e ){
+				accionCifrar();
+			}	
+		});
 
-		panel.add( nombreArchivo );
-		panel.add( btnCifrar );
-		panel.add( btnSeleccionar );
+		cbxCifrar = new JCheckBox("Cifrar");
+		cbxCifrar.addItemListener(new ItemListener(){
+				public void itemStateChanged( ItemEvent e){
+					if( e.getStateChange() == 1){
+						opCifrar = true;
+						opDecifrar = false;
+						cbxDecifrar.setSelected(false);
+					}
+				}
+		});
+
+		cbxDecifrar = new JCheckBox("Decifrar");
+		cbxDecifrar.addItemListener(new ItemListener(){
+				public void itemStateChanged( ItemEvent e){
+					if( e.getStateChange() == 1){
+						opCifrar = false;
+						opDecifrar = true;
+						cbxCifrar.setSelected(false);
+					}
+				}
+		});
+
+		panelSuperior.add( lblTitulo);
+		panelArchivo.add( lblNombreArchivo );
+		panelArchivo.add( btnSeleccionar );
+	
+		GroupLayout gl = new GroupLayout(panelControles);
+		panelControles.setLayout( gl );
+		gl.setHorizontalGroup(
+			gl.createSequentialGroup()
+				.addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
+					.addComponent( cbxCifrar )
+					.addComponent( btnOperacion )	
+				)
+				.addGroup(gl.createParallelGroup(GroupLayout.Alignment.TRAILING)
+					.addComponent( cbxDecifrar )
+				)	
+		);	
+
+		gl.setVerticalGroup(
+				gl.createSequentialGroup()
+				.addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
+					.addGap(5)
+					.addComponent( cbxCifrar )
+					.addComponent( cbxDecifrar )	
+				)
+				.addGap(5)
+				.addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
+					.addComponent( btnOperacion )
+				)	
+		);	
+
+	//	panelControles.add(cbxCifrar);
+	//	panelControles.add(cbxDecifrar);
+	//	panelControles.add(btnCifrar);
 
 		getContentPane().setLayout(new BorderLayout());
-		getContentPane().add( panel, BorderLayout.CENTER );
+		getContentPane().add( panelSuperior, BorderLayout.NORTH );
+		getContentPane().add( panelArchivo, BorderLayout.CENTER );
+		getContentPane().add( panelControles, BorderLayout.LINE_END );
 	}
 
 	public void accionSalir(){
@@ -70,7 +130,10 @@ public class UiAlicia extends JFrame{
 		if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
 			archivo = fc.getSelectedFile();
 			seleccionado = true;
-			nombreArchivo.setText(archivo.getName());
+			lblNombreArchivo.setText(archivo.getName());
+		}else{	
+			seleccionado = false;
+			lblNombreArchivo.setText("Selecciona un archivo");
 		}
 	}
 
